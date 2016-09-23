a1041d13384eefdbad8ce0c1d3f47fedbd419464
James Mitchell <jdm3@st-and.ac.uk>
2016-09-23 14:52:58 +0100

    Fix numbers.sh

--- log/numbers.sh
+++ log/numbers.sh
@@ -3,12 +3,17 @@
 # Simple script to extract the actual numbers produced that are in log files, 
 # e.g. 
 #
-# log/numbers.sh log/laptop/jones 
+# log/numbers.sh log/lovelace/jones 
 #
 # will create file log/laptop/jones.txt with the number in the jones* log 
 # files in the directory log/laptop.
 
 set -e
+
+if [ -f $1.txt ]; then
+  rm -f $1.txt
+fi
+touch $1.txt
 for file in $1*
 do
    tail -1 $file >> $1.txt
